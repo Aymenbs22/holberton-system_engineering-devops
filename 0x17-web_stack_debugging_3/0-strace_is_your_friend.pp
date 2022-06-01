74e0e145fff5b8fd454b74811301152f4c085669
Aymenbs22 <aymenbenslima1@gmail.com>
2022-06-01 22:45:25 +0100

    Puppet Script That fix error (500 Internal Server Error)

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# create a file
-exec { 'add':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+# Puppet Script That fix error (500 Internal Server Error)
+exec { 'flask':
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider => 'shell',
 }
