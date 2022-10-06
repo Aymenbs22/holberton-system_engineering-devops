44659bad7b8994df6f18ba68287df51be472eb8d
Aymenbs22 <aymenbenslima1@gmail.com>
2022-10-06 23:26:45 +0100

    encrees open file limit for nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,9 @@
 # encrees open file limit for nginx
+# restart
 exec { 'encrees-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-  }
+}
 
 exec { 'restart':
   command => 'nginx restart',
