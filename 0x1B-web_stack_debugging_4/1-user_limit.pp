3cc3a1644562a7cc6568ac08404a0feef6296408
Aymenbs22 <aymenbenslima1@gmail.com>
2022-10-24 16:49:25 +0100

    puppet script that change the user holberton limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 #puppet script that change holberton user limit to 5000
 
 exec { 'holberton user fix limitt':
-  command => 'sed -i "s/holberton/hard/nofile/5000" /etc/security/limits.conf',
+  command => 'sed -i "s/holberton/hard/nofile/5000/holberton/soft/nofile/5000" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
+
