e9f288561e9f18c2e57870d635d3f39aa240dfb5
Aymenbs22 <aymenbenslima1@gmail.com>
2022-10-24 16:01:10 +0000

    Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 #puppet script that change holberton user limit to 5000
 
-exec { 'holberton-user-fix-limitt':
+exec { 'holberton-user-hard-fix-limitt':
   command => 'sed -i "s/holberton/hard/nofile/50000" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 #puppet script that change holberton user limit to 5000
-exec { 'holberton-user-fix-limitt':
+exec { 'holberton-user-soft-fix-limitt':
   command => 'sed -i "s/holberton/soft/nofile/50000" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
